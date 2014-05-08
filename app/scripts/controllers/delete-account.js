3b0232e42bd27764adb4fa322ba44a868095c5d3
Louis Wilbrink Air <louis.wilbrink@gmail.com>
2014-05-07 23:55:23 -0400

    improved delete-account view, added scope references

--- app/scripts/controllers/delete-account.js
+++ app/scripts/controllers/delete-account.js
@@ -2,9 +2,17 @@
 
 angular.module('rememoirApp')
   .controller('DeleteAccountCtrl', function ($scope) {
+
     $scope.deleteAccount = {
 
-      // Model.
+      email: '',
+      
+      password : '',
 
+      deleteAccount: function () {
+
+        console.log('call that service:', this.email, this.password);
+
+      }
     };
   });
